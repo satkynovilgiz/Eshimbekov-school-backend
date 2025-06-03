886eff9c7537ee8b00a4fb50b08d49916c6c016c
Ilgiz Satkynov <satkynovilgiz008@gmail.com>
2025-06-03 18:28:13 +0600

    oppo

--- eshimbekov/urls.py
+++ eshimbekov/urls.py
@@ -3,15 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect('/admin/')),
     path('admin/', admin.site.urls),
     path('api/news/', include('news.urls')),
     path('api/teachers/', include('teachers.urls')),
     path('api/banners/', include('banner.urls')),
     path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('', lambda request: JsonResponse({"message": "Welcome to Eshimbekov School API"})),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
