a3a23acd62702bbe2f9e5f3a56c84b642f1bc2e4
Ilgiz Satkynov <satkynovilgiz008@gmail.com>
2025-06-03 18:09:38 +0600

    create new admin panel

--- eshimbekov/urls.py
+++ eshimbekov/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from news.views import NewsListView, NewsDetailView
+from django.views.static import serve
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path('', lambda request: HttpResponseRedirect('/admin/')),
     path('admin/', admin.site.urls),
-    path('api/news/', NewsListView.as_view(), name='news-list'),
-    path('api/news/<int:pk>/', NewsDetailView.as_view(), name='news-detail'),
+    path('api/news/', include('news.urls')),
     path('api/teachers/', include('teachers.urls')),
-    path('api/banners/', include('banner.urls')),  # правильный путь
+    path('api/banners/', include('banner.urls')),
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
