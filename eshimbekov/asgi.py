879b3b48f372d5866041f617abc77b85831d9112
Ilgiz Satkynov <satkynovilgiz008@gmail.com>
2025-06-16 19:42:14 +0600

    fix: created banner

--- eshimbekov/asgi.py
+++ eshimbekov/asgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eshimbekov.settings')
-
 application = get_asgi_application()
